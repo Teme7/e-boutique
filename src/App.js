98e6d5fc239d53266d6d9026426c3f7e14f73aba
Teme Sahle <tsahle@terradatum.com>
2022-06-13 18:52:01 -0400

    build persistent navbar using react router and outlet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
+
+// just for experimentation
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h2>Here is the navbar!!</h2>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
+const Cart = () => {
+  return <h1 style={{background: 'olive', color: 'pink'}}>Welcome to your shopping cart!</h1>
+};
+
+const AboutUs = () => {
+  return <h3 style={{ background: 'skyblue', color: 'goldenrod', padding: '20px', border: 'solid' }}>How our nitch biz came into existence...</h3>
+} 
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route>
+        <Route path='/home' element={<Home />} />
+        <Route path='cart' element={<Cart />} />
+        <Route path='about-us' element={<AboutUs />} />
+      </Route>
     </Routes>
   )
 };
