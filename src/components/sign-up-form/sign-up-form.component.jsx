419f6602011e932975dbdf22a2d52f71a982b76b
Teme Sahle <tsahle@terradatum.com>
2022-06-20 15:42:49 -0400

    sign up form now able to create user using email/password combo, form fields reset once user is authenticated and created inside db

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+import { createAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
+import { createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
 const defaultFormFields = {
   displayName: '',
   email: '',
@@ -20,10 +23,36 @@ const SignUpForm = () => {
     setFormFields({ ...formFields, [name]: value });
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if(password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(email, password);
+
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+    } catch(error) {
+        if(error.code === 'auth/email-already-in-use') {
+          alert("Cannot create user; email already in use.");
+        } else {
+          console.error("User creation encountered an error.", error)
+        }
+    }
+  }
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  }
+
   return (
     <>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit}>
         <label>Display Name</label>
         <input 
           type='text' 
