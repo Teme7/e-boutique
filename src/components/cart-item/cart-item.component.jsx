ba97f410487591929ec6ed0c70b4339bbca1b72b
Teme Sahle <tsahle@terradatum.com>
2022-07-08 14:45:58 -0400

    styled-components added to CartItem component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,21 @@
-import './cart-item.styles.scss';
+import { 
+  CartItemContainer, 
+  ItemDetails, 
+  Name, 
+  ImageContainer 
+} from './cart-item.styles';
 
 const CartItem = ({ cartItem }) => {
   const { imageUrl, name, price, quantity } = cartItem;
 
   return (
-    <div className='cart-item-container'>
-      <img src={imageUrl} alt={`${name}`} />
-      <div className='item-details'>
-        <span className='name'>{name}</span>
-        <span className='price'>{quantity} X ${price}</span>
-      </div>
-    </div>
-    
+    <CartItemContainer>
+      <ImageContainer src={imageUrl} alt={`${name}`}></ImageContainer>
+      <ItemDetails>
+        <Name>{name}</Name>
+        <span>{quantity} X ${price}</span>
+      </ItemDetails>
+    </CartItemContainer>
   )
 }
 
