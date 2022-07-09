1f2142446e87f9d61452c5b50f5a58c624d4e3c8
Teme Sahle <tsahle@terradatum.com>
2022-07-09 01:08:36 -0400

    utilize the power of styled-components library to target and style ShoppingIcon, two diff ways of achieving the same outcome are commented out

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,8 @@
 import { useContext } from 'react';
 
-import { ReactComponent as ShoppingICon } from '../../assets/shopping-bag.svg';
 import { CartContext } from '../../contexts/cart.context';
 
-import { CartIconContainer, ItemCount } from './cart-icon.styles';
+import { ShoppingICon, CartIconContainer, ItemCount } from './cart-icon.styles';
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
