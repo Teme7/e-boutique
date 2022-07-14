f803690a3cb3d31800c3d81a349e1c6cc727863a
Teme Sahle <tsahle@terradatum.com>
2022-07-13 20:45:46 -0400

    utilize useNavigate hook to implement routing front-end links to their particular category pages

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 import { BackgroundImage, DirectoryBody, DirectoryItemContainer } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <DirectoryBody>
         <h2>{title}</h2>
